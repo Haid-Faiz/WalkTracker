14e456b3854dcff7c0702c3aec48c4a733ca2dc8
Faizan Haider <haiderfaizan732@gmail.com>
2021-05-23 22:33:08 +0530

    Dagger-Hilt Dependency Injection setup done

--- app/src/main/java/com/example/walktracker/ui/MainActivity.kt
+++ app/src/main/java/com/example/walktracker/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.walktracker
+package com.example.walktracker.ui
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,8 +7,16 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.walktracker.R
+import com.example.walktracker.db.RunDao
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    private lateinit var dao: RunDao  // dagger will look for RunDao recipe in one of our Module & will inject it here
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +26,11 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_dashboard
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
